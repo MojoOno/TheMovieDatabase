0ba2faa1d42fa2baab11f5d377cb0e62ae39c478
MojoOno <Ffranck0406@gmail.com>
2025-02-25 15:01:13 +0100

    Added entity class for Actor. I've added matching attributes to the DTO, and am in doubt if everything should be here or not - but would rather exclude them in plenum tomorrow

--- src/main/java/dat/entities/Actor.java
+++ src/main/java/dat/entities/Actor.java
@@ -1,6 +1,8 @@
 package dat.entities;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 
 @Entity
@@ -12,6 +14,22 @@ import lombok.*;
 public class Actor
 {
     @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
+    private String name;
+    private String originalName;
+    private int gender;
+    private String knownForDepartment;
+    private double popularity;
+    private String profilePath;
+    private int castId;
+    private String character;
+    private String creditId;
+
+    @ManyToOne
+    @ToString.Exclude
+    @Setter
+    private Movie movie;
+
 
 }
