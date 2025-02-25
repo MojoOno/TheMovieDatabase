5f3aefb88c3a9509b5b136954103801c58f82988
AndyTheDragon <andre.jansam@gmail.com>
2025-02-25 13:16:30 +0100

    Removed unused import statement

--- src/main/java/dat/services/MovieService.java
+++ src/main/java/dat/services/MovieService.java
@@ -1,6 +1,5 @@
 package dat.services;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import dat.dtos.GenreDTO;
