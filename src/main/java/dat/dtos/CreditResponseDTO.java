542a1815e32df48af2cd743c47b36325bfa5e61f
Falcho <m.k.falcham@gmail.com>
2025-02-26 14:34:24 +0100

    Added jsonignoreproperties

--- src/main/java/dat/dtos/CreditResponseDTO.java
+++ src/main/java/dat/dtos/CreditResponseDTO.java
@@ -1,10 +1,12 @@
 package dat.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonIgnoreProperties (ignoreUnknown = true)
 public class CreditResponseDTO
 {
     private int id;
