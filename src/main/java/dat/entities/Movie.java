b61796bc6ebbae2c6ec30d68bb77abb24281649e
Falcho <m.k.falcham@gmail.com>
2025-02-26 14:34:05 +0100

    Added attributes to movie, outcommented relations to try and persist movies into DB

--- src/main/java/dat/entities/Movie.java
+++ src/main/java/dat/entities/Movie.java
@@ -1,5 +1,6 @@
 package dat.entities;
 
+import dat.dtos.MovieDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -20,26 +21,37 @@ public class Movie
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
     private String title;
+    private String description;
     @Column(name = "release_date")
     private LocalDate releaseDate;
     private double rating;
     private double popularity;
 
-    @EqualsAndHashCode.Exclude
-    @OneToMany (mappedBy = "movie", fetch = FetchType.EAGER)
-    private Set<Credit> credits = new HashSet<>();
-
-    @EqualsAndHashCode.Exclude
-    @ManyToMany (mappedBy = "movies", fetch = FetchType.EAGER)
-    private Set<Genre> genres = new HashSet<>();
-
-    public void addCredit(Credit credit)
+    public Movie(MovieDTO movieDTO)
     {
-        if (credit != null)
-        {
-            credits.add(credit);
-            credit.setMovie(this);
-        }
+        this.id = movieDTO.getId();
+        this.title = movieDTO.getTitle();
+        this.description = movieDTO.getDescription();
+        this.releaseDate = movieDTO.getReleaseDate();
+        this.rating = movieDTO.getScore();
+        this.popularity = movieDTO.getPopularity();
     }
+
+//    @EqualsAndHashCode.Exclude
+//    @OneToMany (mappedBy = "movie", fetch = FetchType.EAGER)
+//    private Set<Credit> credits = new HashSet<>();
+//
+//    @EqualsAndHashCode.Exclude
+//    @ManyToMany (mappedBy = "movies", fetch = FetchType.EAGER)
+//    private Set<Genre> genres = new HashSet<>();
+//
+//    public void addCredit(Credit credit)
+//    {
+//        if (credit != null)
+//        {
+//            credits.add(credit);
+//            credit.setMovie(this);
+//        }
+//    }
 
 }
