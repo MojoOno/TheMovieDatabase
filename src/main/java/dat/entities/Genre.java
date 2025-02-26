aaeb23f96a0778f543bb01380bb217b371324d57
AndyTheDragon <andre.jansam@gmail.com>
2025-02-26 08:02:20 +0100

    Feature: genre skal indeholde ne liste af movies har også tilføjet en metode til at tilføje movies til listen. Der skal laves en design beslutning om listen skal være et Set, så vi ikke kan have dubletter. Der skal laves en design beslutning om metoden også skal tilføje genren til filmen, hvilket er fint hvis genreList i Movie klassen er et Set (så kommer der ikke dubletter), men kan være problematisk hvis det bare er en liste. Som det er nu, så skal man manuelt tilføje genren på film objektet.

--- src/main/java/dat/entities/Genre.java
+++ src/main/java/dat/entities/Genre.java
@@ -5,7 +5,10 @@ import dat.dtos.GenreDTO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.*;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -20,10 +23,21 @@ public class Genre
     private Long id;
     private int genreId;
     private String name;
+    @ManyToMany(mappedBy = "movies")
+    private List<Movie> movies; //skal måske være et Set? Så vi ikke har den samme film to gange i listen
 
     public Genre(GenreDTO genreDTO)
     {
         this.genreId = genreDTO.getId();
         this.name = genreDTO.getName();
+    }
+
+    public void addMovie(Movie movie)
+    {
+        if (movie != null)
+        {
+            movies.add(movie);
+            //movie.addGenre(this);
+        }
     }
 }
