5fdbf1ae54718a41a08a028ada61b916fe9b877b
Falcho <m.k.falcham@gmail.com>
2025-02-26 10:25:52 +0100

    Updatede movie entity

--- src/main/java/dat/entities/Movie.java
+++ src/main/java/dat/entities/Movie.java
@@ -1,7 +1,14 @@
 package dat.entities;
+
+import dat.entities.Actor;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import lombok.*;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -13,5 +20,16 @@ public class Movie
 {
     @Id
     private Long id;
+    private String title;
+
+    @OneToMany
+    private List<Actor>actorList;
+
+    @OneToMany
+    private List<Director>directorList;
+
+    @ManyToMany
+    private List<Genre>genreList;
+
 
 }
