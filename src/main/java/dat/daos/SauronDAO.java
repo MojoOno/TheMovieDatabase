368c6de893797b176a80c28d486da9e7444dc66e
AndyTheDragon <andre.jansam@gmail.com>
2025-02-27 21:53:44 +0100

    Why do we need three practically identical DAO classes, when we can just use this one?

---
+++ src/main/java/dat/daos/SauronDAO.java
@@ -0,0 +1,128 @@
+package dat.daos;
+
+import dat.exceptions.ApiException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+import java.util.List;
+
+public class SauronDAO
+{
+    private static EntityManagerFactory emf;
+    private static SauronDAO instance;
+
+    private SauronDAO(EntityManagerFactory emf)
+    {
+        this.emf = emf;
+    }
+
+    public static SauronDAO getInstance(EntityManagerFactory emf)
+    {
+        if (instance == null)
+        {
+            instance = new SauronDAO(emf);
+        }
+        return instance;
+    }
+
+    public <T> T create(T object)
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            em.getTransaction().begin();
+            em.persist(object);
+            em.getTransaction().commit();
+            return object;
+        }
+        catch (Exception e)
+        {
+            throw new ApiException(401, "Error persisting object to db. ", e);
+        }
+    }
+
+    public <T> List<T> create(List<T> objects)
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            em.getTransaction().begin();
+            for (T object : objects)
+            {
+                em.persist(object);
+            }
+            em.getTransaction().commit();
+            return objects;
+        }
+        catch (Exception e)
+        {
+            throw new ApiException(401, "Error persisting object to db. ", e);
+        }
+    }
+
+    public <T> T read(Class<T> type, int id)
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            return em.find(type, id);
+        }
+        catch (Exception e)
+        {
+            throw new ApiException(401, "Error reading object from db", e);
+        }
+    }
+
+    public <T> List<T> findAll(Class<T> type)
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            return em.createQuery("SELECT t FROM " + type.getSimpleName() + " t", type).getResultList();
+        }
+        catch (Exception e)
+        {
+            throw new ApiException(401, "Error reading objects from db", e);
+        }
+    }
+
+    public <T> T update(T object)
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            em.getTransaction().begin();
+            T updatedEntity = em.merge(object);
+            em.getTransaction().commit();
+            return updatedEntity;
+        }
+        catch (Exception e)
+        {
+            throw new ApiException(401, "Error updating object. ", e);
+        }
+    }
+
+    public <T> void delete(T object)
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            em.getTransaction().begin();
+            em.remove(object);
+            em.getTransaction().commit();
+        }
+        catch (Exception e)
+        {
+            throw new ApiException(401, "Error deleting object. ", e);
+        }
+    }
+
+    public <T> void delete(Class<T> type, int id)
+    {
+        try (EntityManager em = emf.createEntityManager())
+        {
+            em.getTransaction().begin();
+            T object = em.find(type, id);
+            em.remove(object);
+            em.getTransaction().commit();
+        }
+        catch (Exception e)
+        {
+            throw new ApiException(401, "Error deleting object. ", e);
+        }
+    }
+}
