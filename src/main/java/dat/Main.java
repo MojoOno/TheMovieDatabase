fa7c7e1d0b3fd4f2748d662df1daf333cdc86e4d
AndyTheDragon <andre.jansam@gmail.com>
2025-02-27 10:21:28 +0100

    hent genres fra API og persist dem i db.

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -4,13 +4,16 @@ import dat.config.HibernateConfig;
 import dat.daos.CreditDAO;
 import dat.daos.GenreDAO;
 import dat.daos.MovieDAO;
+import dat.dtos.GenreDTO;
 import dat.dtos.MovieDTO;
+import dat.entities.Genre;
 import dat.entities.Movie;
 import dat.services.MovieService;
 import dat.utils.DataAPIReader;
 import jakarta.persistence.EntityManagerFactory;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main
 {
@@ -24,6 +27,13 @@ public class Main
 
         DataAPIReader reader = new DataAPIReader();
         MovieService service = new MovieService(reader);
+
+        List<GenreDTO> genreDTOs = service.getGenres();
+        List<Genre> genres = genreDTOs.stream()
+                .map(Genre::new)
+                .toList();
+        genres.forEach(System.out::println);
+        genreDAO.create(genres);
 
         List<MovieDTO> movieDTOs = service.getMoviesFromCountryFromLastFiveYears("da");
 
