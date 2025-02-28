340fd4ef800268e020f945c6f95f48a3309c77a7
Falcho <m.k.falcham@gmail.com>
2025-02-28 11:20:29 +0100

    Removed unused imports

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -1,20 +1,6 @@
 package dat;
 
-import dat.config.HibernateConfig;
-import dat.daos.CreditDAO;
-import dat.daos.GenreDAO;
-import dat.daos.MovieDAO;
-import dat.dtos.CreditDTO;
-import dat.dtos.GenreDTO;
-import dat.dtos.MovieDTO;
-import dat.entities.Credit;
-import dat.entities.Genre;
-import dat.entities.Movie;
 import dat.services.MovieService;
-import dat.utils.DataAPIReader;
-import jakarta.persistence.EntityManagerFactory;
-
-import java.util.List;
 
 public class Main
 {
